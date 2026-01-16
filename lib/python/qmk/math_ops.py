69dfdec4e4ee6d35712a83f1177628e72412c393
Panayiotis Constantinou <panayiotis.s.constantinou@gmail.com>
2026-01-16 21:09:12 +0200

    keyboard(miryoku): add mouse jitter

--- lib/python/qmk/math_ops.py
+++ lib/python/qmk/math_ops.py
@@ -2,11 +2,20 @@
 
 Gratefully copied from https://stackoverflow.com/a/9558001
 """
+
 import ast
 import operator as op
 
 # supported operators
-operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul, ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor, ast.USub: op.neg}
+operators = {
+    ast.Add: op.add,
+    ast.Sub: op.sub,
+    ast.Mult: op.mul,
+    ast.Div: op.truediv,
+    ast.Pow: op.pow,
+    ast.BitXor: op.xor,
+    ast.USub: op.neg,
+}
 
 
 def compute(expr):
@@ -19,7 +28,7 @@ def compute(expr):
     >>> compute('1 + 2*3**(4^5) / (6 + -7)')
     -5.0
     """
-    return _eval(ast.parse(expr, mode='eval').body)
+    return _eval(ast.parse(expr, mode="eval").body)
 
 
 def _eval(node):
